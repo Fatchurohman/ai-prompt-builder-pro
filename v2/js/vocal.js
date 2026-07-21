82e5a14e8b62a8809c3c7c45778e3abf83946d07
Fatchurohman <fatur62@gmail.com>
2026-07-21 07:57:45 +0700

    Update vocal.js

--- v2/js/vocal.js
+++ v2/js/vocal.js
@@ -1,1 +1,32 @@
-
+const vocalList = [
+    {
+        gender: "Male",
+        type: "Baritone",
+        character: "Deep warm baritone male vocal"
+    },
+    {
+        gender: "Male",
+        type: "Tenor",
+        character: "Powerful emotional tenor male vocal"
+    },
+    {
+        gender: "Male",
+        type: "Bass",
+        character: "Deep cinematic bass male vocal"
+    },
+    {
+        gender: "Female",
+        type: "Soprano",
+        character: "Bright expressive female soprano vocal"
+    },
+    {
+        gender: "Female",
+        type: "Alto",
+        character: "Warm emotional female alto vocal"
+    },
+    {
+        gender: "Female",
+        type: "Mezzo Soprano",
+        character: "Rich powerful female mezzo vocal"
+    }
+];
