fd3a739e0159b77eb1836c76b8012c166c35883b
Fatchurohman <fatur62@gmail.com>
2026-07-21 08:00:59 +0700

    Update sound.js

--- v2/js/sound.js
+++ v2/js/sound.js
@@ -1,1 +1,12 @@
-
+const soundList = [
+    "Warm Analog Sound",
+    "Modern Digital Production",
+    "Cinematic Atmosphere",
+    "Deep Bass Powerful",
+    "Crystal Clear Vocal",
+    "Vintage 80s Recording",
+    "Live Concert Feel",
+    "Dark Moody Sound",
+    "Epic Orchestral Sound",
+    "Radio Ready Professional Mix"
+];
