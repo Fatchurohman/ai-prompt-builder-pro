6a1268b6fbef877f184cb4bb120f44b0f502cb02
Fatchurohman <fatur62@gmail.com>
2026-07-21 07:56:00 +0700

    Update instrument.js

--- v2/js/instrument.js
+++ v2/js/instrument.js
@@ -1,1 +1,44 @@
+const instrumentList=[
 
+"Select Instrument",
+
+"Piano",
+"Acoustic Guitar",
+"Electric Guitar",
+"Bass Guitar",
+"Ukulele",
+
+"Violin",
+"Cello",
+"Orchestra Strings",
+
+"Flute",
+"Saxophone",
+"Trumpet",
+
+"Kendang",
+"Gamelan",
+"Seruling Bambu",
+
+"Drums",
+"808 Bass",
+
+"Synth",
+"Pad",
+"Arpeggiator"
+
+];
+
+const instrumentSelect=document.getElementById("instrument");
+
+instrumentList.forEach(item=>{
+
+const option=document.createElement("option");
+
+option.value=item;
+
+option.textContent=item;
+
+instrumentSelect.appendChild(option);
+
+});
